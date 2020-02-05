3a644e39221abaa0c6bb646c605289fa47a0a446
klevocky <k.levocky@gmail.com>
2020-02-05 16:17:10 +0100

    sekvencia

--- cvic01/Src/main.c
+++ cvic01/Src/main.c
@@ -17,24 +17,23 @@ int main(void)
 {
 	uint32_t index=0;
 
-	uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+	//uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+	uint32_t sekvencia = 0b10101001110111011100101010000000;
+
 
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
 
 	GPIOA->BSRR = (1<<5); // set
 
-	for(;;){
-		//GPIOA->ODR ^= (1<<5); // toggle
-		//for (i = 0; i < 100000; i++) {}
-		if (pole[index] == 1){GPIOA->BSRR = (1<<5);} //set
-		else if (pole[index] == 0){GPIOA->BRR = (1<<5);} //reset
+	while(1){
 
-		index++;
-
-		if (index>= sizeof(pole)) index=0;
+	for(index=0;index<32;index++){
+		if ((sekvencia << index)&(0x80000000)){GPIOA->BSRR = (1<<5);}
+		else {GPIOA->BRR = (1<<5);} //reset
 
 		for (volatile uint32_t i = 0; i < 100000; i++) {}
 	}
+}
 }
 
