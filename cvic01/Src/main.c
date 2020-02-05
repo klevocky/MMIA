3edd677df0f415a2d25d2763fdd441a3774e3ded
klevocky <k.levocky@gmail.com>
2020-02-05 15:17:41 +0100

    2

--- cvic01/Src/main.c
+++ cvic01/Src/main.c
@@ -11,7 +11,19 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+#include <stm32f0xx.h>
+
 int main(void)
 {
-	for(;;);
+	volatile uint32_t i;
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
+	//GPIOA->BSRR = (1<<5); // set
+
+	for(;;){
+		GPIOA->ODR ^= (1<<5); // toggle
+		for (i = 0; i < 100000; i++) {}
+}
 }
