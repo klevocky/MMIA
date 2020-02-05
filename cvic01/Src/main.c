035d92eb545d88481e58a713f3e3b0196566639b
klevocky <k.levocky@gmail.com>
2020-02-05 15:53:04 +0100

    sos

--- cvic01/Src/main.c
+++ cvic01/Src/main.c
@@ -15,15 +15,26 @@
 
 int main(void)
 {
-	volatile uint32_t i;
+	uint32_t index=0;
+
+	uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
 
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
 
-	//GPIOA->BSRR = (1<<5); // set
+	GPIOA->BSRR = (1<<5); // set
 
 	for(;;){
-		GPIOA->ODR ^= (1<<5); // toggle
-		for (i = 0; i < 100000; i++) {}
+		//GPIOA->ODR ^= (1<<5); // toggle
+		//for (i = 0; i < 100000; i++) {}
+		if (pole[index] == 1){GPIOA->BSRR = (1<<5);} //set
+		else if (pole[index] == 0){GPIOA->BRR = (1<<5);} //reset
+
+		index++;
+
+		if (index>= sizeof(pole)) index=0;
+
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	}
 }
-}
+
