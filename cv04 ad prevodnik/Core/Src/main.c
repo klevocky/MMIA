3c8d10bf33005ca576e1ee9405332db3b1f6902c
klevocky <k.levocky@gmail.com>
2020-02-26 15:31:33 +0100

    okok

--- cv04 ad prevodnik/Core/Src/main.c
+++ cv04 ad prevodnik/Core/Src/main.c
@@ -33,6 +33,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define ADC_Q 12;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -61,12 +62,12 @@ static void MX_ADC_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 static volatile uint32_t raw_pot;
+static volatile uint32_t avg_pot;
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
  raw_pot = HAL_ADC_GetValue(hadc);
 
  static uint32_t avg_pot;
- static uint32_t ADC_Q;
  raw_pot = avg_pot >> ADC_Q;
  avg_pot -= raw_pot;
  avg_pot += HAL_ADC_GetValue(hadc);
