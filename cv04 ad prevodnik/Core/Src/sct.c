5f3f6b00400611e5f10a96ed5253d5d3ebdab583
klevocky <k.levocky@gmail.com>
2020-02-26 15:01:49 +0100

    aaa

---
+++ cv04 ad prevodnik/Core/Src/sct.c
@@ -0,0 +1,104 @@
+/*
+ * sct.c
+ *
+ *  Created on: Feb 19, 2020
+ *      Author: student
+ */
+#include <stm32f0xx.h>
+#include "sct.h"
+#include "main.h"
+
+
+void sct_init(void)
+{
+	sct_led(0);
+}
+
+void sct_led(uint32_t value)
+{
+
+	for (uint32_t i = 0; i < 32; i++)
+	{
+		if (value & 1)
+		{
+			HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, 1);
+		}
+		else
+		{
+			HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, 0);
+		}
+		value >>= 1;
+
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1);
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
+	}
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 1);
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 0);
+}
+
+static const uint32_t reg_values[4][10] = {
+		{
+				//PCDE--------GFAB @ DIS1
+				0b0111000000000111 << 16,
+				0b0100000000000001 << 16,
+				0b0011000000001011 << 16,
+				0b0110000000001011 << 16,
+				0b0100000000001101 << 16,
+				0b0110000000001110 << 16,
+				0b0111000000001110 << 16,
+				0b0100000000000011 << 16,
+				0b0111000000001111 << 16,
+				0b0110000000001111 << 16,
+		},
+		{
+				//----PCDEGFAB---- @ DIS2
+				0b0000011101110000 << 0,
+				0b0000010000010000 << 0,
+				0b0000001110110000 << 0,
+				0b0000011010110000 << 0,
+				0b0000010011010000 << 0,
+				0b0000011011100000 << 0,
+				0b0000011111100000 << 0,
+				0b0000010000110000 << 0,
+				0b0000011111110000 << 0,
+				0b0000011011110000 << 0,
+		},
+		{
+				//PCDE--------GFAB @ DIS3
+				0b0111000000000111 << 0,
+				0b0100000000000001 << 0,
+				0b0011000000001011 << 0,
+				0b0110000000001011 << 0,
+				0b0100000000001101 << 0,
+				0b0110000000001110 << 0,
+				0b0111000000001110 << 0,
+				0b0100000000000011 << 0,
+				0b0111000000001111 << 0,
+				0b0110000000001111 << 0,
+		},
+		{
+				//PCDE--------GFAB @ LED
+				0b0000000000000000 << 0,
+				0b0000001000000000 << 0,
+				0b0000011000000000 << 0,
+				0b0000111000000000 << 0,
+				0b0000111100000000 << 0,
+				0b0000111110000000 << 0,
+				0b0000111111000000 << 0,
+				0b0000111111100000 << 0,
+				0b0000111111110000 << 0,
+				0b0000000000000000 << 0,
+				},
+};
+
+void sct_value(uint16_t value)
+{
+	uint32_t reg = 0;
+	uint32_t led = 0;
+	reg |= reg_values[0][value / 100 % 10];
+	reg |= reg_values[1][value / 10 % 10];
+	reg |= reg_values[2][value % 10];
+	reg |= reg_values[3][led];
+
+	sct_led(reg);
+}
